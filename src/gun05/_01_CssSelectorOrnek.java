944950093e7d7ad4eed87f3ce2af69d5d0df13fa
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-07-19 01:39:28 +0300

    ###

--- src/gun05/_01_CssSelectorOrnek.java
+++ src/gun05/_01_CssSelectorOrnek.java
@@ -4,6 +4,15 @@ package gun05;
         import Utils.BaseStaticDriver;
         import org.openqa.selenium.By;
         import org.openqa.selenium.WebElement;
+/*1- https://formsmarts.com/form/yu?mode=h5 sitesini açın
+2- Business i çekleyin.  [id$='4586_0']   [type='radio'][value='Business']
+3- discover XYZ ye tıklatın ve online Advertising i seçin
+ [id='u_1fG_4588']   [value='Online Advertising']
+4- Every day i seçin   [id='u_1fG_89585_0']
+5- Good u seçin  [type='radio'][value='Good']
+6- using XYZ yi tıklatın ve 3.seçeneği seçin
+[id='u_1fG_4597']     [value*='month'] [id='u_1fG_4597'] > :nth-child(4)
+*/
 
 public class _01_CssSelectorOrnek extends BaseStaticDriver {
     public static void main(String[] args) {
@@ -13,22 +22,22 @@ public class _01_CssSelectorOrnek extends BaseStaticDriver {
         WebElement business=driver.findElement(By.cssSelector("[id$='4586_0']"));
         business.click();
         Bekle(2);
-        WebElement dropDownDiscover=driver.findElement(By.cssSelector("[id='u_1fG_4588']"));
+        WebElement dropDownDiscover=driver.findElement(By.cssSelector("[id$='_4588']"));
         dropDownDiscover.click();
         Bekle(2);
-        WebElement optionOnlineAdvertising=driver.findElement(By.cssSelector("[value='Online Advertising']"));
+        WebElement optionOnlineAdvertising=driver.findElement(By.cssSelector("[id$=_4588] > [value='Online Advertising']"));
         optionOnlineAdvertising.click();
         Bekle(2);
-        WebElement buttonEveryDay=driver.findElement(By.cssSelector("[id='u_1fG_89585_0']"));
+        WebElement buttonEveryDay=driver.findElement(By.cssSelector("[id='u_eva_89585_0'][value='Every Day']"));
         buttonEveryDay.click();
         Bekle(2);
         WebElement goodRadio=driver.findElement(By.cssSelector("[type='radio'][value='Good']"));
         goodRadio.click();
         Bekle(2);
-        WebElement howLongDropdown=driver.findElement(By.cssSelector("[id='u_1fG_4597']"));
+        WebElement howLongDropdown=driver.findElement(By.cssSelector("[id$='_4597']"));
         howLongDropdown.click();
         Bekle(2);
-        WebElement secenek4=driver.findElement(By.cssSelector("[id='u_1fG_4597'] > :nth-child(4)"));
+        WebElement secenek4=driver.findElement(By.cssSelector("[id$='_4597'] > :nth-child(4)"));
         secenek4.click();
 
         goodRadio.click(); // sadece son açılan dropdown kapansın diye herhangi bir yere tıklattık
