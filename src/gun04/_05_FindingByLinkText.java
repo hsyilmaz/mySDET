8b8a8405bded889d4bda5ba79633ae317c74f3b3
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-07-18 11:43:22 +0300

    bismillah

--- src/gun04/_05_FindingByLinkText.java
+++ src/gun04/_05_FindingByLinkText.java
@@ -14,8 +14,11 @@ public class _05_FindingByLinkText extends BaseStaticDriver {
         System.out.println("element.getAttribute(\"href\") = " + element.getAttribute("href"));
         System.out.println("element.getAttribute(\"rel\") = " + element.getAttribute("rel"));
 
-        WebElement element2 = driver.findElement(By.partialLinkText("Süper Fiyat")); //sadece a taglerinde calisir
+        WebElement element2 = driver.findElement(By.partialLinkText("Süper Teklif")); //sadece a taglerinde calisir
         System.out.println("element2.getText() = " + element2.getText());
+
+        WebElement element3 = driver.findElement(By.linkText("Yurt Dışından"));
+        System.out.println("element3.getText() = " + element3.getText());
 
 
 
