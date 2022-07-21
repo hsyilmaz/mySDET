4bad8099febeeaf5b4a7f68e39209376f2587b66
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-07-21 17:45:41 +0300

    gunsonu+++

---
+++ src/gun07/_10.java
@@ -0,0 +1,30 @@
+package gun07;
+
+import Utils.BaseStaticDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class _10 extends BaseStaticDriver {
+    public static void main(String[] args) {
+        driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+
+        List<WebElement> countries=driver.findElements(By.cssSelector("[id^='box10']"));
+        for(WebElement ctry: countries)
+            System.out.println(ctry.getText());
+
+        List<WebElement>capitals=driver.findElements(By.cssSelector("[id^='box'][dragableelement]"));
+        for (WebElement city:capitals)
+            System.out.println(city.getText());
+
+        Actions aksyiyonlar=new Actions(driver);
+        for(int i=0; i<7;i++){
+        Action aksiyon=aksyiyonlar.dragAndDrop(countries.get(i),capitals.get(i)).build();
+        aksiyon.perform();}
+
+        BekleKapat();
+    }
+}
