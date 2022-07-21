c8a3aedf3712caf855cc553b2b786643dd769ee0
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-07-22 00:39:12 +0300

    gunsonu+++

--- src/gun08/_02_YonTuslariSecim.java
+++ src/gun08/_02_YonTuslariSecim.java
@@ -1,7 +1,6 @@
 package gun08;
 
 import Utils.BaseStaticDriver;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -10,25 +9,23 @@ import org.openqa.selenium.interactions.Actions;
 
 public class _02_YonTuslariSecim extends BaseStaticDriver {
     public static void main(String[] args) {
-        driver.get("https://demoqa.com/auto-complete");
+       driver.get("https://demoqa.com/auto-complete");
 
-        WebElement element = driver.findElement(By.id("autoCompleteSingleInput"));
+        WebElement element=driver.findElement(By.id("autoCompleteSingleInput"));
 
         Actions aksiyonlar=new Actions(driver);
-        Action action=aksiyonlar
+
+        Action aksiyon = aksiyonlar
                 .moveToElement(element)
                 .click()
                 .sendKeys("b")
                 .build();
-        action.perform();
+        aksiyon.perform();
 
         Bekle(2);
-        action=aksiyonlar.sendKeys(Keys.DOWN).sendKeys(Keys.ENTER).build();
-        action.perform();
+        aksiyon = aksiyonlar.sendKeys(Keys.DOWN).sendKeys(Keys.ENTER).build();
+        aksiyon.perform();
 
-
-        BekleKapat();
-
-
+       BekleKapat();
     }
 }
