764dcc6ef28baf702888eb5b1955df99b44a7e74
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-07-22 15:26:50 +0300

    gunsonu+++

--- src/gun07/_10.java
+++ src/gun07/_10.java
@@ -5,30 +5,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-
-import java.util.Collections;
 import java.util.List;
-
 public class _10 extends BaseStaticDriver {
     public static void main(String[] args) {
         driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
 
-        List<WebElement> countries=driver.findElements(By.cssSelector("[id^='box10']"));
-        for(WebElement ctry: countries)
+        List<WebElement> countries = driver.findElements(By.cssSelector("[id^='box10']"));
+        for (WebElement ctry : countries)
             System.out.println(ctry.getText());
-        countries.getClass();
 
-        List<WebElement>capitals=driver.findElements(By.cssSelector("[id^='box'][dragableelement]"));
-        for (WebElement city:capitals)
+        List<WebElement> capitals = driver.findElements(By.cssSelector("[id^='box'][dragableelement]"));
+        for (WebElement city : capitals)
             System.out.println(city.getText());
 
-        Actions aksiyonlar=new Actions(driver);
-         for(int i=0; i<7;i++){
-         Action aksiyon=aksiyonlar.dragAndDrop(countries.get(i),capitals.get(i)).build();
-         aksiyon.perform();}
 
-        BekleKapat();
-
+        for (WebElement city : capitals)
+            for (WebElement ctry : countries) {
+                Actions aksiyonlar = new Actions(driver);
+                Action aksiyon = aksiyonlar.dragAndDrop(city, ctry).build();
+                aksiyon.perform();
+            }
 
     }
+
 }
