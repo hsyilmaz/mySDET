a7ee3f72d8d6df108486914d654e9a729e8bbe9a
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-08-05 21:57:58 +0300

    guncelleme

---
+++ src/gun21_Ag5_Fproject/senario2.java
@@ -0,0 +1,34 @@
+package gun21_Ag5_Fproject;
+
+import Utils.BaseStaticDriver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class senario2 extends BaseStaticDriver {
+    public static void main(String[] args) {
+        driver.get("http://demowebshop.tricentis.com/");
+
+        WebElement loginbtn = driver.findElement(By.linkText("Log in"));
+        loginbtn.click();
+
+        WebElement email = driver.findElement(By.id("Email"));
+        email.sendKeys("grup21@hotmail.com");
+
+        WebElement Password = driver.findElement(By.id("Password"));
+        Password.sendKeys("grup212");
+
+        WebElement loginbtn2 = driver.findElement(By.cssSelector("[value='Log in']"));
+        loginbtn2.click();
+
+        WebElement hatamsj=driver.findElement(By.xpath("//*[contains(text(),'was unsuccessful')]"));
+
+        Assert.assertTrue(hatamsj.getText().contains("unsuccessful"));
+
+        Assert.assertTrue(hatamsj.isDisplayed());
+
+        BekleKapat();
+
+    }
+
+}
